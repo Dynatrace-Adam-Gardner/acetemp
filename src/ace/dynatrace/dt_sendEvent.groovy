807cf9d7f1042770210c962439d2c50c68898040
Adam Gardner <adam.gardner@dynatrace.com>
2020-03-22 09:36:29 +1000

    Update dt_sendEvent.groovy

--- src/ace/dynatrace/dt_sendEvent.groovy
+++ src/ace/dynatrace/dt_sendEvent.groovy
@@ -24,8 +24,29 @@ def dt_sendEvent( Map args ) {
    String strTimeframe;
  */
  
- String strKeptnURL = args.containsKey("strKeptnURL") ? args.strKeptnURL : "${KEPTN_URL}"
- // TODO - Others...
+ String strKeptnURL = args.containsKey("strKeptnURL") ? args.strKeptnURL : "DEFAULT";
+ String strKeptnAPIToken = args.containsKey("strKeptnAPIToken") ? args.strKeptnURL : "${KEPTN_API_TOKEN}";
+ String strKeptnProject = args.containsKey("strKeptnURL") ? args.strKeptnURL : "${KEPTN_PROJECT}";
+ String strKeptnService = args.containsKey("strKeptnURL") ? args.strKeptnURL : "${KEPTN_SERVICE}";
+ String strKeptnStage = args.containsKey("strKeptnURL") ? args.strKeptnURL : "${KEPTN_STAGE}";
+ String strKeptnEventType = args.containsKey("strKeptnURL") ? args.strKeptnURL : "";
+ String strStartTime = args.containsKey("strKeptnURL") ? args.strKeptnURL : "";
+ String strEndTime = args.containsKey("strKeptnURL") ? args.strKeptnURL : "";
+ String strTimeframe = args.containsKey("strKeptnURL") ? args.strKeptnURL : "";
+ boolean bDebug = args.containsKey("debug") ? args.strKeptnURL : false;
  
- echo "[dt_sendEvent.groovy] Keptn URL is: " + strKeptnURL;
+ echo "[dt_sendEvent.groovy] Debug Mode: " + bDebug;
+ 
+ if (bDebug) {
+   echo "[dt_sendEvent.groovy] Keptn URL is: " + strKeptnURL;
+   echo "[dt_sendEvent.groovy] Keptn API Token is: " + strKeptnAPIToken;
+   echo "[dt_sendEvent.groovy] Keptn Project is: " + strKeptnProject;
+   echo "[dt_sendEvent.groovy] Keptn Service is: " + strKeptnService;
+   echo "[dt_sendEvent.groovy] Keptn Stage is: " + strKeptnStage;
+   echo "[dt_sendEvent.groovy] Keptn Event Type is: " + strKeptnEventType;
+   echo "[dt_sendEvent.groovy] Start Time is: " + strStartTime;
+   echo "[dt_sendEvent.groovy] End Time is: " + strEndTime;
+   echo "[dt_sendEvent.groovy] Timeframe is: " + strTimeframe;
+ }
+ 
 }
